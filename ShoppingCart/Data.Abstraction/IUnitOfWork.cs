eeff9b74b0c7563a1085c9b0e7c908b163f03755
andreiserdulet <serdulet.a2017@yahoo.com>
2021-09-08 17:47:04 +0300

    first commit

---
+++ ShoppingCart/Data.Abstraction/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Data.Abstraction
+{
+    public interface IUnitOfWork
+    {
+        IRepository<T> GetRepository<T>() where T: BaseEntityModel;
+        Task<bool> SaveChangesAsync();
+    }
+}
